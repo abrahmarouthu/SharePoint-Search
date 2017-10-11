1a2e6f332a1564e1de7273d06de3e199ab9ecf15
bashwin.87 <bashwin.87@gmail.com>
2017-10-11 11:52:03 -0700

    Dummy Copy
    Dummy Copy

--- Sharepoint-Search - Copy/src/app/DailyTasks/task-detail.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    templateUrl: './task-detail.component.html',
-    styleUrls: ['./task-detail.component.css']
-})
-export class TasksDetailComponent {
-    
-}
